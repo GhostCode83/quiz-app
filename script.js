2a3117e1adf07386a5783b2b9cf29b9706d59379
Randy Douglas <randy.e.douglas@gmail.com>
2020-07-20 14:00:24 -0400

    final draft

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const STORE = [
   {
     question: "Question 1: <br>Where did Frank Castle and Amy first meet?",
-    image: '<img src="images/question-page/frank-amy-waiting-room.jpg" alt="frank-and-amy-in-waiting-room" width="500">'
+    image: '<img src="images/question-page/frank-amy-waiting-room.jpg" alt="frank-and-amy-in-waiting-room">'
     ,
     options: [
       'a bar',
@@ -14,7 +14,7 @@ const STORE = [
   },
   {
     question: "Question 2: <br>Who was Frank Castle's one call when he was arrested?",
-    image: '<img src="images/question-page/frank-arrested.jpg" alt="frank-mugshot" width="500">',
+    image: '<img src="images/question-page/frank-arrested.jpg" alt="frank-mugshot">',
 
     options: [
       'Russo',
@@ -27,7 +27,7 @@ const STORE = [
   },
   {
     question: "Question 3: <br>What's the bounty placed on Frank Castle's head?",
-    image: '<img  src="images/question-page/bounty-hunters.gif" alt="bounty-hunters-agains-frank" width="500">',
+    image: '<img  src="images/question-page/bounty-hunters.gif" alt="bounty-hunters-agains-frank">',
     options: [
       '$5 Billion',
       '$5 Thousand',
@@ -39,7 +39,7 @@ const STORE = [
   },
   {
     question: "Question 4: <br>How did Krista's father die?",
-    image: '<img  src="images/question-page/krista-young.png" alt="krista-sad-as-child-on-floor" width="500">',
+    image: '<img  src="images/question-page/krista-young.png" alt="krista-sad-as-child-on-floor">',
     options: [
       'Heart Attack',
       'Shot by the Punisher',
@@ -51,7 +51,7 @@ const STORE = [
   },
   {
     question: "Question 5: <br>Why did Krista befriend Madani?",
-    image: '<img  src="images/question-page/krista-offers-madani-accepts-drink.jpg" alt="krista-offers-madani-accepts-drink" width="500">',
+    image: '<img  src="images/question-page/krista-offers-madani-accepts-drink.jpg" alt="krista-offers-madani-accepts-drink">',
     options: [
       'To chat about Russo',
       'She appreciated her as a professional',
@@ -83,32 +83,36 @@ function displayStartPage() {
 function displayQuestionPage() {
   //loads a multiple choice question with a submit button
   if (currentQuestion < STORE.length) {
-    return `<form class="question js-question group"> 
-              <h2> 
+    return `<form class="question js-question"> 
+              <h2 > 
               ${STORE[currentQuestion].question} 
               </h2>
-              <div class="item"> 
-              ${STORE[currentQuestion].image} 
-              </div>
-              <div class="item">
-                <input id="option 1" type="radio" name="option" class="option" value="0" required> 
-                <label for="option 1">${STORE[currentQuestion].options[0]}</label>
-                </br>
-                <input id="option 2" type="radio" name="option" class="option" value="1"> 
-                <label for="option 2">${STORE[currentQuestion].options[1]}</label>
-                </br>
-                <input id="option 3" type="radio" name="option" class="option" value="2"> 
-                <label for="option 3">${STORE[currentQuestion].options[2]}</label>
-                </br>
-                <input id="option 4" type="radio" name="option" class="option" value="3"> 
-                <label for="option 4">${STORE[currentQuestion].options[3]}</label>
-                </br>
-                <button id="submit" class="option" type='submit' >Submit Answer</button>
-              </div>
-              
-              <div class="item">
-              <p>Points earned: ${ score}</p>
-              </div>
+              <section class="group">
+                <div class="item"> 
+                ${STORE[currentQuestion].image} 
+                </div>
+                
+                <div class="item align">
+                  <input id="option 1" type="radio" name="option" class="option" value="0" required> 
+                  <label for="option 1">${STORE[currentQuestion].options[0]}</label>
+                  </br>
+                  <input id="option 2" type="radio" name="option" class="option" value="1"> 
+                  <label for="option 2">${STORE[currentQuestion].options[1]}</label>
+                  </br>
+                  <input id="option 3" type="radio" name="option" class="option" value="2"> 
+                  <label for="option 3">${STORE[currentQuestion].options[2]}</label>
+                  </br>
+                  <input id="option 4" type="radio" name="option" class="option" value="3"> 
+                  <label for="option 4">${STORE[currentQuestion].options[3]}</label>
+                  </br>
+                  <button id="submit" class="option" type='submit' >Submit Answer</button>
+                </div>
+                
+                <div class="item align">
+                <p>Points earned: </p>
+                <p class="score"> ${ score} </p>
+                </div>
+              </section>
             </form>`
   }
 }
@@ -118,17 +122,30 @@ function displayFeedbackPage() {
   if (STORE[currentQuestion].correct) {
     return `<form class="js-feedback-page"> 
     <h2>That's right!</h2>
-    <img src="images/answer-page/frank-winning-gun-fight.jpg" alt="frank-winning-gun-fight" width="500"> 
-    <p>Points earned: ${ score}</p>
-    <button class="feedback-page">Next Question</button>
+    <section class="group">
+      <div class="item">
+        <img src="images/answer-page/frank-winning-gun-fight.jpg" alt="frank-winning-gun-fight" width="500"> 
+      </div>
+      <div class="item item-2">
+        <p>Points earned: </p>
+        <p class="score">${ score}</p>
+        <button class="feedback-page">Continue</button>
+      </div>
     </form>`
   } else {
     return `<form class="js-feedback-page"> 
     <h2>Wrong answer!</h2>
-    <img src="images/answer-page/frank-beside-car-with-gun.jpg" alt="frank-pinned-down-by-gunfire-behind-car" width="500">
-    <p>The correct answer was: <br> ${STORE[currentQuestion].options[STORE[currentQuestion].answer]} </p>
-    <p>Points earned: ${ score}</p>
-    <button class="feedback-page">Next Question</button>
+    <section class="group">
+      <div class="item">
+        <img src="images/answer-page/frank-beside-car-with-gun.jpg" alt="frank-pinned-down-by-gunfire-behind-car" width="500">
+      </div>
+      <div class="item item-2">
+        <p>The correct answer was: <br> ${STORE[currentQuestion].options[STORE[currentQuestion].answer]} </p>
+        <p>Points earned: </p>
+        <p class="score">${ score}</p>
+        <button class="feedback-page">Continue</button>
+      </div>
+    </section>  
     </form>`
   }
 }
@@ -146,10 +163,16 @@ function displayResultsPage() {
   } else {
     return `<form class="js-results-page">
     <h2>Results</h2>
-    <h3>Are you even trying?</h3>
-    <p>You scored: ${score} / ${STORE.length} </p>              
+    <section class="group">
+    <div class="item">
     <img src="images/last-page/frank-and-amy-dissatisfied.jpg" alt="frank-and-amy-dissatisfied-at-storefront" width="500">
-    <input type="submit" value="try again">
+    </div>
+    <div class="item">
+      <h3>Are you even trying?</h3>
+      <p>You scored: </p> 
+      <p class="score"> ${score} / ${STORE.length} </p>              
+      <input type="submit" value="try again">
+    </div>
     </form>`
   }
 }
